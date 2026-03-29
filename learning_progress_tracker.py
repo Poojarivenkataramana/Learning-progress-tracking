7207ea5df5591f98f12d11f6f5a7bc6de0ca590f
Poojari venkataramana <poojarivenkataramana1@gmail.com>
2026-03-29 18:59:04 +0530

    Update learning_progress_tracker.py

--- learning_progress_tracker.py
+++ learning_progress_tracker.py
@@ -23,3 +23,17 @@ A Python project to track and visualize coding consistency using Matplotlib.
 1. Install required library:
 ```bash
 pip install matplotlib
+
+
+2. Run The script:
+  python Learning-progress-tracking.py
+
+3. Sample input:
+  Days: 1 2 3 4 5 6 7
+  Hours:2 4 3 5 6 7 2
+
+4. Output(output.png)
+
+#Key insight
+  This project helps identify productivity patterns and improve consistency over time.
+It highlights peak performance days and low productivity periods, helping in better planning.
